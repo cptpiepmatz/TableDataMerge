bc3a3ff0e8da3752b0f35c1c7d95e78214354c6e
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-06-08 18:12:53 +0200

    Fixed Easy Installer
    Fresh Install did not work

--- EasySetup.ps1
+++ EasySetup.ps1
@@ -1,29 +1,33 @@
 $source = "https://github.com/derPiepmatz/TableDataMerge/releases/latest/download/TableDataMerge.exe"
-$installPath = "$Home\AppData\Local\TableDataMerge"
+$installPath = "$env:LOCALAPPDATA\TableDataMerge"
 $outFile = "$installPath\TableDataMerge.exe"
 $tdmOutput = "$installPath\Output"
-$tdmLink = "$Home\Desktop\TDM"
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$tdmLink = "$DesktopPath\TDM"
 $shortcutArguments = @("-latex", "-latex -mathmode", "-latex -hline")
 
 # create install directory
 if (-Not (Test-Path -Path $installPath -PathType Container)) {
+  Write-Output "Creating Install Directory"
   New-Item -Path "$Home\AppData\Local" -Name "TableDataMerge" -ItemType "directory"
 }
 
 # ensure you have the newest version downloaded
+Write-Output "Ensuring you have the newest Version"
 if (Test-Path -Path $outFile -PathType Leaf) {
   Remove-Item -Path $outFile
 }
 Invoke-WebRequest -Uri $source -OutFile $outFile
 
 # create shortcuts on desktop
+Write-Output "Creating Shortcuts on your Desktop"
 foreach ($args in $shortcutArguments) {
   if (-Not (Test-Path -Path "$tdmLink $args.lnk" -PathType Leaf)) {
     $WshShell = New-Object -comObject Wscript.Shell
     $Shortcut = $WshShell.CreateShortcut("$tdmLink $args.lnk")
-    $Shortcut.TargetPath = $outFile
+    $Shortcut.TargetPath = "$outFile"
     $Shortcut.Arguments = "$args -nopause"
-    $Shortcut.WorkingDirectory = $tdmOutput
+    $Shortcut.WorkingDirectory = "$tdmOutput"
     $Shortcut.Save()
   }
 }
@@ -34,19 +38,19 @@ if (-Not(Test-Path -Path "$installPath/dirIcon.ico" -PathType Leaf)) {
   Invoke-WebRequest -Uri $dirIconSource -OutFile "$installPath/dirIcon.ico"
   attrib +h "$installPath/dirIcon.ico"
 }
-if (-Not (Test-Path -Path "$tdmLink Output.lnk" -PathType Leaf)) {
-  $WshShell = New-Object -comObject Wscript.Shell
-  $Shortcut = $WshShell.CreateShortcut("$tdmLink Output.lnk")
-  $Shortcut.TargetPath = $tdmOutput
-  $Shortcut.IconLocation = "$installPath/dirIcon.ico"
-  $Shortcut.Working
-  $Shortcut.Save()
-}
 if (-Not (Test-Path -Path $tdmOutput -PathType Container)) {
   New-Item -Path "$installPath" -Name "Output" -ItemType "directory"
 }
+if (-Not (Test-Path -Path "$tdmLink Output.lnk" -PathType Leaf)) {
+  $WshShell = New-Object -comObject Wscript.Shell
+  $Shortcut = $WshShell.CreateShortcut("$tdmLink Output.lnk")
+  $Shortcut.TargetPath = "$tdmOutput"
+  $Shortcut.IconLocation = "$installPath/dirIcon.ico"
+  $Shortcut.Save()
+}
 
 # refresh shell
+Write-Output "Refreshing Shell"
 $code = @'
   [System.Runtime.InteropServices.DllImport("Shell32.dll")]
   private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
@@ -58,3 +62,5 @@ $code = @'
 
 Add-Type -MemberDefinition $code -Namespace WinAPI -Name Explorer
 [WinAPI.Explorer]::Refresh()
+
+Write-Output "Hopefully done..."
