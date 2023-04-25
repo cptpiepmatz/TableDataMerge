53453abe5a1a37a6fd70747c2665fccbb2a54de8
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-04-25 08:34:30 +0200

    removed c++ code

--- src/functions/countEntries.h
+++
@@ -1,8 +0,0 @@
-#ifndef TABLEDATA_COUNTENTRIES_H
-#define TABLEDATA_COUNTENTRIES_H
-
-#include "../stdLibraries.h"
-
-void countEntries(int &width, int &depth, ifstream &file, const string &fileType);
-
-#endif //TABLEDATA_COUNTENTRIES_H
