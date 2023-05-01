29ee7187e149615bf63b7078cf54a8506aea50d0
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-01 17:17:50 +0200

    will build icon for windows exe

---
+++ build.rs
@@ -0,0 +1,13 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("icon/icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {
+}
