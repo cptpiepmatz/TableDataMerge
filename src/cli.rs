b5eb66761dcb606d43e726cb38a41d5aa2561a27
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-01 16:32:58 +0200

    updated args to include suffix and prefix

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,9 @@
-use std::fmt::{Display, Formatter};
+use crate::table::cell::Cell;
+use crate::util::AnyRange;
 use clap::error::ErrorKind;
 use clap::{CommandFactory, Parser, ValueEnum};
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 
 #[derive(Parser, Debug, Clone)]
 pub struct Args {
@@ -22,14 +25,17 @@ pub struct Args {
     #[arg(short, long, default_value_t = false)]
     pub sign: bool,
 
-    #[arg(short, long = "mathmode", default_value_t = false)]
-    pub math_mode: bool,
-
     #[arg(short = 'H', long, default_value_t = false)]
     pub hline: bool,
 
-    #[arg(long, long, default_value = ",")]
+    #[arg(short, long, default_value = ",")]
     pub csv_sep: String,
+
+    #[arg(short = 'P', long, num_args(1), value_parser = parse_fix)]
+    pub prefix: Vec<(AnyRange<usize>, String)>,
+
+    #[arg(short = 'S', long, num_args(1), value_parser = parse_fix)]
+    pub suffix: Vec<(AnyRange<usize>, String)>,
 
     #[arg(short, long, default_value_t = false)]
     pub vertical: bool,
@@ -44,12 +50,35 @@ pub enum OutTypes {
     Dat,
     Tex,
     Md,
-    Json
+    Json,
 }
 
 #[derive(ValueEnum, Debug, Copy, Clone, Default)]
 pub enum DecimalSeparator {
     #[default]
     Dot,
-    Comma
+    Comma,
+}
+
+#[derive(Debug)]
+struct ParseFixError;
+
+impl Display for ParseFixError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Error for ParseFixError {}
+
+fn parse_fix(input: &str) -> Result<(AnyRange<usize>, String), ParseFixError> {
+    let mut split = input.splitn(2, ':');
+
+    // TODO: handle this better
+    let range = split.next().unwrap();
+    let range: AnyRange<usize> = range.parse().unwrap();
+
+    let fix = split.next().unwrap();
+
+    Ok((range, fix.to_string()))
 }
