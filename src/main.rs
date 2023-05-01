deacff3a9dd004c2e42fee9d50e54103f49b01e1
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-01 10:32:28 +0200

    fixed prefix

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn main() {
                 OutTypes::Dat => basename += ".dat",
                 OutTypes::Tex => basename += ".tex",
             }
-            String::from("tdm") + basename.as_str()
+            String::from("tdm_") + basename.as_str()
         }
         Some(arg) => arg.to_string(),
     };
