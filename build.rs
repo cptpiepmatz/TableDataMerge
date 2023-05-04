1390534a7ddd81fedae9a00191808f8e5cad6140
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-04 18:56:43 +0200

    added build.rs conditions

--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@ extern crate winres;
 
 #[cfg(windows)]
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=Cargo.lock");
+    println!("cargo:rerun-if-changed=icon/icon.ico");
     let mut res = winres::WindowsResource::new();
     res.set_icon("icon/icon.ico");
     res.compile().unwrap();
