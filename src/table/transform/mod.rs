0fc74f332c3405d4bc1bee4d7ed2d9a5ebeaf8d6
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-04 15:12:04 +0200

    added trait impls for ParseTableError

--- src/table/transform/mod.rs
+++ src/table/transform/mod.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use crate::table::transform::csv::ParseCsvTableError;
 use crate::table::transform::json::ParseJsonTableError;
 use crate::table::transform::m::ParseMTableError;
@@ -9,8 +11,22 @@ pub mod m;
 pub mod md;
 pub mod tex;
 
+/// Error if parsing tables fail.
+#[derive(Debug)]
 pub enum ParseTableError {
     Csv(ParseCsvTableError),
     Json(ParseJsonTableError),
     M(ParseMTableError),
 }
+
+impl Display for ParseTableError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseTableError::Csv(c) => c.fmt(f),
+            ParseTableError::Json(j) => j.fmt(f),
+            ParseTableError::M(m) => m.fmt(f)
+        }
+    }
+}
+
+impl Error for ParseTableError {}
